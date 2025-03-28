9597cf26598984a66bc8016d5830341c8e8ce67b
E1ijahJ <elijahguijoseph@gmail.com>
2025-03-27 23:39:35 -0400

    Added the final function

--- src/PointerFunctions.cpp
+++ src/PointerFunctions.cpp
@@ -14,3 +14,15 @@ std::array<uint8_t, 4> floatToBytes(float value) {
 int callFunctionPointer(int (*func)(int), int value) {
     return func(value);
 }
+
+std::array<uint8_t, 4> getSelectedBytesFromDouble(double value) {
+    std::array<uint8_t, 4> selectedBytes;
+    uint8_t* raw = reinterpret_cast<uint8_t*>(&value);
+    
+    selectedBytes[0] = raw[0];
+    selectedBytes[1] = raw[2];
+    selectedBytes[2] = raw[4];
+    selectedBytes[3] = raw[6];
+
+    return selectedBytes;
+}
